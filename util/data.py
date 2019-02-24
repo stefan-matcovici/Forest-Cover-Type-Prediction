7dcae324fb542d3d4d5c5cbdb7665645ac6b3745
andrluc <andrluc@amazon.com>
2019-02-24 14:42:37 +0200

    Added Task1 partial notebook

--- util/data.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pandas as pd
-
-
-def load_data(path):
-    train_csv_path = os.path.join(path, "train.csv")
-    test_csv_path = os.path.join(path, "test.csv")
-    return pd.read_csv(train_csv_path), pd.read_csv(test_csv_path)
