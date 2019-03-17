4295f50e845beec25483dcb3d5ad6e46002dffae
andrluc <andrluc@amazon.com>
2019-03-17 15:30:32 +0200

    Added ground truth comparasion for the univariated outlier detector

--- util/outliers_statistics.py
+++ util/outliers_statistics.py
@@ -1,18 +1,16 @@
-def outliers_statistics(outliers_ids):
-    import scipy.io
-    
-    mat = scipy.io.loadmat('../data/cover.mat')
-    
-    outliers_mapping = zip(mat["X"][:, 0], mat["y"][:, 0])
-    
+def outliers_statistics(train_data, outliers_ids):
     entry_ids_mapping = {
-        "outliers_ids": [id for id, is_outlier in outliers_mapping if is_outlier],
-        "non_outliers_ids": [id for id, is_outlier in outliers_mapping if not is_outlier]
+        "outliers_ids": train_data[train_data['target'] == 1].index.values.tolist(),
+        "non_outliers_ids": train_data[train_data['target'] == 0].index.values.tolist()
     }
-    
+ 
     TP = len(set(entry_ids_mapping["outliers_ids"]) & set(outliers_ids))
     FP = len(set(entry_ids_mapping["non_outliers_ids"]) & set(outliers_ids))
     FN = len(set(entry_ids_mapping["outliers_ids"]) - set(outliers_ids))
+    
+    print("TP: " + str(TP))
+    print("FP: " + str(FP))
+    print("FN: " + str(FN))
     
     if TP + FP > 0:
         precision = TP / (TP + FP)
