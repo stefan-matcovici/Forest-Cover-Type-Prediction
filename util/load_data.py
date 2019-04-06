52918b1ef2bb8910bc5a68be0243b4884514e621
andrluc <andrluc@amazon.com>
2019-04-06 13:39:48 +0300

    Finished SVM classifier

--- util/load_data.py
+++ util/load_data.py
@@ -9,24 +9,48 @@ def load_data(path, aggregate=True):
     train_csv_path = os.path.join(path, "train.csv")
     test_csv_path = os.path.join(path, "test.csv")
     train_data, test_data = pd.read_csv(train_csv_path), pd.read_csv(test_csv_path)
-
-    if aggregate:
-        train_data = aggregate_categorical_column(train_data, "Soil")
-        train_data = aggregate_categorical_column(train_data, "Wilderness")
-
-        test_data = aggregate_categorical_column(test_data, "Soil")
-        test_data = aggregate_categorical_column(test_data, "Wilderness")
-
-        categorical_attributes = ['Soil_Type', 'Wilderness_Type']
-    else:
-        categorical_attributes = []
-
     attributes = train_data.columns.tolist()
     target = ['Cover_Type']
+    
+    train_data_aggregated = aggregate_categorical_column(train_data, "Soil")
+    train_data_aggregated = aggregate_categorical_column(train_data_aggregated, "Wilderness")
+
+    test_data_aggregated = aggregate_categorical_column(test_data, "Soil")
+    test_data_aggregated = aggregate_categorical_column(test_data_aggregated, "Wilderness")
+
+    categorical_attributes = ['Soil_Type', 'Wilderness_Type']
+
+    attributes = train_data_aggregated.columns.tolist()
+
     numerical_attributes = np.setdiff1d(attributes, categorical_attributes + target + ["Id"]).tolist()
 
+    # outliers removal
+    first_quantiles = train_data_aggregated[numerical_attributes].quantile(0.25)
+    third_quantiles = train_data_aggregated[numerical_attributes].quantile(0.75)
+
+    outlier_ids = []
+
+    for idx, numerical_attribute in enumerate(numerical_attributes):
+        first_quantile = first_quantiles[numerical_attribute]
+        third_quantile = third_quantiles[numerical_attribute]
+
+        iqr = third_quantile - first_quantile
+
+        without_outlier_df = train_data_aggregated[train_data_aggregated[numerical_attribute] > first_quantile - 1.5*iqr]
+        without_outlier_df = without_outlier_df[without_outlier_df[numerical_attribute] < third_quantile + 1.5*iqr]
+
+        outliers = train_data_aggregated[~train_data_aggregated.isin(without_outlier_df).all(1)]
+        outlier_ids += outliers['Id'].values.tolist()
+    
+    if aggregate:
+        train_data_without_outliers = train_data_aggregated[~train_data_aggregated['Id'].isin(outlier_ids)]
+    else:
+        categorical_attributes = []
+        numerical_attributes = attributes
+        train_data_without_outliers = train_data[~train_data['Id'].isin(outlier_ids)]
+        
     return {
-        "train_data": train_data,
+        "train_data": train_data_without_outliers,
         "test_data": test_data,
         "attributes": attributes,
         "categorical_attributes": categorical_attributes,
